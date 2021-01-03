6cd0dbebc0ce59ea1ab4d88955505b6639dc0481
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-01-03 21:02:57 +0700

    add node-sass

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import styles from './Home.module.scss';
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = ({}) => {
-  return <h1>Home</h1>;
+  return (
+    <div className={styles.home}>
+      <h1 className={styles.title}>그림</h1>
+      <form action=''>
+        <svg
+          className={styles.searchIcon}
+          fill='none'
+          stroke='currentColor'
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-width='2'
+            d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+          ></path>
+        </svg>
+        <input type='search' name='nim' id='nim' />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
