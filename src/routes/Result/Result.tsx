d5a68c2095160e58f49142be8e5e8be799c2cd02
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-01-06 17:55:26 +0700

    add reset.css

--- src/routes/Result/Result.tsx
+++ src/routes/Result/Result.tsx
@@ -5,24 +5,41 @@ interface ResultProps {}
 
 const Result: React.FC<ResultProps> = ({}) => {
   return (
-    <div className='result'>
-      <div className='picture'>
+    <div className={styles.result}>
+      <div className={styles.picture}>
         <img
-          src='https://akademik.unikom.ac.id/foto/10118087.jpg'
-          alt='10118087'
+          src='https://akademik.unikom.ac.id/foto/10118244.jpg'
+          alt='10118244'
         />
       </div>
-      <div className='caption'>
-        <h1 className='nim'></h1>
+      <div className={styles.caption}>
+        <h1 className={styles.nim}>#10118244</h1>
         <div>
-          <div className='label'>major</div>
-          <div className='info'>teknik informatika</div>
+          <div className={styles.label}>major</div>
+          <div className={styles.info}>teknik informatika</div>
         </div>
         <div>
-          <div className='label'>year of entry</div>
-          <div className='info'>2020</div>
+          <div className={styles.label}>year of entry</div>
+          <div className={styles.info}>2020</div>
         </div>
       </div>
+      <button className={styles.button}>
+        <svg
+          className={styles.backIcon}
+          fill='none'
+          stroke='currentColor'
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            d='M15 19l-7-7 7-7'
+          ></path>
+        </svg>
+        back
+      </button>
     </div>
   );
 };
