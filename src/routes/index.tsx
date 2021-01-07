cdae574a1c85d451c03084a16910226d7bcc115d
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-01-07 10:43:27 +0700

    fix loader position

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,13 +5,22 @@ import Loader from 'react-loader-spinner';
 const Home = lazy(() => import('./Home/Home'));
 const Result = lazy(() => import('./Result/Result'));
 
+const SuspenseFallback = (
+  <div
+    style={{
+      marginTop: '4rem',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <Loader type='ThreeDots' color='#00BFFF' height={80} width={80} />
+  </div>
+);
+
 const Routes = () => (
   <Router>
-    <Suspense
-      fallback={
-        <Loader type='ThreeDots' color='#00BFFF' height={80} width={80} />
-      }
-    >
+    <Suspense fallback={SuspenseFallback}>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/result' component={Result} />
